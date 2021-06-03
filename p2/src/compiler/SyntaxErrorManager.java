66f2e5105b621c2fb449a29d92d0ac5d8c73227d
oumist <45634091+oumist@users.noreply.github.com>
2021-06-03 22:12:41 +0100

    syntax error mas especifico y con limite de tokens esperados

--- p2/src/compiler/SyntaxErrorManager.java
+++ p2/src/compiler/SyntaxErrorManager.java
@@ -4,13 +4,23 @@ package compiler;
  * Clase encargada de manejar los errores sintacticos del lenguaje minileng
  * **/
 public class SyntaxErrorManager {
-	public static void printSyntaxError(Token nextToken, int[][] expectedTokenSequences,String[] tokenImage) {
+	public static void printSyntaxError(Token nextToken, int[][] expectedTokenSequences,String[] tokenImage, String informer) {
 	    StringBuilder s = new StringBuilder();
-	      for (int i = 0; i < expectedTokenSequences.length; i++) {
+	    if (expectedTokenSequences.length > 4) {
+	      for (int i = 0; i < 4; i++) {
 	        for (int j = 0; j < expectedTokenSequences[i].length; j++) {
-	          s.append(tokenImage[expectedTokenSequences[i][j]]).append("\t");
+	          	s.append(tokenImage[expectedTokenSequences[i][j]]).append("\t");
 	        }
 	      }
-	      System.out.println("ERROR SINTACTICO en la linea " + nextToken.beginLine + ", en la columna  " + nextToken.beginColumn + ". Encontrado: "  + tokenImage[nextToken.kind] + ", esperado: " + s);
+	    }
+	    else {
+	      for (int i = 0; i < expectedTokenSequences.length; i++) {
+	        for (int j = 0; j < expectedTokenSequences[i].length; j++) {
+	          	s.append(tokenImage[expectedTokenSequences[i][j]]).append("\t");
+	        }
+	      }
+	    }
+	    System.out.println("ERROR SINTACTICO debido a " + informer + " en la linea " + nextToken.beginLine + ", en la columna  " + nextToken.beginColumn + ". Encontrado: "  + tokenImage[nextToken.kind] + ", esperado: " + s);
 	}
 }
+
